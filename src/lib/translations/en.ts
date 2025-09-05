06d2e60546bd61041e36ca3437e1702e5a7d4be8
Игорь Тормышев <tormyhseviv@MacBook-Air-Igor.local>
2025-09-05 15:21:10 +0300

    chore: bump version to 0.0.18
    fix: Обновил интерфейс. Исправил сканирование. Добавил сборку для linux.

--- src/lib/translations/en.ts
+++ src/lib/translations/en.ts
@@ -68,7 +68,7 @@ export const en = {
   checking: "Checking",
 
   // About Tab
-  version: "Version 0.0.17",
+  version: "Version 0.0.18",
   powerfulNetworkTool: "Network tool for discovering and managing 3D printers",
 
   // Language Tab
