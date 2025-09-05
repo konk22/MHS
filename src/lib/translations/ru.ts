6b392285505e3af0d48075f8701e3bab2dd50852
Игорь Тормышев <tormyhseviv@MacBook-Air-Igor.local>
2025-09-05 16:00:32 +0300

    chore: bump version to 0.0.23
    fix: Обновил интерфейс. Исправил сканирование. Добавил сборку для linux.

--- src/lib/translations/ru.ts
+++ src/lib/translations/ru.ts
@@ -69,7 +69,7 @@ export const ru = {
   checking: "Проверка",
 
   // About Tab
-  version: "Версия 0.0.22",
+  version: "Версия 0.0.23",
   powerfulNetworkTool: "Сетевой инструмент для поиска и управления 3D принтерами",
 
   // Language Tab
