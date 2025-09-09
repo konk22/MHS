58a75c8630694a68bbfc5e8f2bfab24c4c2ce161
Игорь Тормышев <tormyhseviv@MacBook-Air-Igor.local>
2025-09-09 17:34:35 +0300

    chore: bump version to 0.0.45
    fix: Исправил баг с состоянием cancelled. После сборки перестает работать уведомления.

--- src/lib/translations/ru.ts
+++ src/lib/translations/ru.ts
@@ -70,7 +70,7 @@ export const ru = {
   checking: "Проверка",
 
   // About Tab
-  version: "Версия 0.0.44",
+  version: "Версия 0.0.45",
   powerfulNetworkTool: "Сетевой инструмент для поиска и управления 3D принтерами",
 
   // Language Tab
