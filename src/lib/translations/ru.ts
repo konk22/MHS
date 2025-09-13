d5a3c3a7f0ca12021446a28b19e4ca116f0d5ede
Игорь Тормышев <tormyhseviv@MacBook-Air-Igor.local>
2025-09-13 19:52:48 +0300

    chore: bump version to 0.0.47
    Доработал телеграм бота

--- src/lib/translations/ru.ts
+++ src/lib/translations/ru.ts
@@ -70,7 +70,7 @@ export const ru = {
   checking: "Проверка",
 
   // About Tab
-  version: "Версия 0.0.46",
+  version: "Версия 0.0.47",
   powerfulNetworkTool: "Сетевой инструмент для поиска и управления 3D принтерами",
 
   // Language Tab
